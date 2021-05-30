cbe3bd60bee4be6e547a8764db7dd655340e6875
arkaprovo <apb@live.in>
2021-05-31 05:18:11 +0530

    fixd test exceptions

--- src/test/java/com/experiment/EventResourceTest.java
+++ src/test/java/com/experiment/EventResourceTest.java
@@ -7,15 +7,15 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
+public class EventResourceTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
-                .when().get("/hello-resteasy")
+                .when().get("/events/message/hello")
                 .then()
                 .statusCode(200)
-                .body(is("Hello RESTEasy"));
+                .body(is("message published successfully"));
     }
 
 }
--- src/test/java/com/experiment/NativeEventResourceIT.java
+++ src/test/java/com/experiment/NativeEventResourceIT.java
@@ -3,7 +3,7 @@ package com.experiment;
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
-public class NativeGreetingResourceIT extends GreetingResourceTest {
+public class NativeEventResourceIT extends EventResourceTest {
 
     // Execute the same tests but in native mode.
 }
